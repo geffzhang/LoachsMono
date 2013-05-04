fc12839dffe2175342100a43ace72a391f8b1b41
geffzhang <geffzhang@qq.com>
2013-05-04 13:50:06 +0800

    test wcf service on mono

---
+++ Loachs.Web/HelloService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Loachs.Web
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "HelloService" in code, svc and config file together.
+    public class HelloService : IHelloService
+    {
+        public string DoWork()
+        {
+            return "Hello World";
+        }
+    }
+}
